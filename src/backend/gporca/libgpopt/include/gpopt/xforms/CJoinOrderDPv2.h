41778ba39658498176b133b226d7d94eced947b7
Chris Hajas <chajas@pivotal.io>
2020-04-23 15:25:32 -0700

    Penalize cross joins in Orca's DPv2 xform similarly to real cost model
    Previously, the DPv2 transform would penalize cross joins by multiplying
    the total cardinality by a constant, causing the total cost of the
    entire tree to increase by that constant. For example, if there was a
    relatively small cross join, at the bottom of a tree, that returned 100
    rows and penalized it by 5X, the cardinality of the above join would
    also get multiplied by 5X. This could result in a very small cross
    product near the bottom causing the overall cost of the tree to increase
    by a much larger amount.
    
    Instead, we accumulate the cost separately and use the same constant
    that is used in the cost model during optimization.

--- src/backend/gporca/libgpopt/include/gpopt/xforms/CJoinOrderDPv2.h
+++ src/backend/gporca/libgpopt/include/gpopt/xforms/CJoinOrderDPv2.h
@@ -360,6 +360,8 @@ namespace gpopt
 				// cost of the expression
 				CDouble m_cost;
 
+				CDouble m_cost_penalty;
+
 				SExpressionInfo(
 								CExpression *expr,
 								const SGroupAndExpression &left_child_expr_info,
@@ -369,7 +371,8 @@ namespace gpopt
 								   m_left_child_expr(left_child_expr_info),
 								   m_right_child_expr(right_child_expr_info),
 								   m_properties(properties),
-								   m_cost(0.0)
+								   m_cost(0.0),
+								   m_cost_penalty(0.0)
 				{
 				}
 
@@ -378,7 +381,8 @@ namespace gpopt
 								SExpressionProperties &properties
 								) : m_expr(expr),
 									m_properties(properties),
-									m_cost(0.0)
+									m_cost(0.0),
+									m_cost_penalty(0.0)
 				{
 				}
 
@@ -388,7 +392,10 @@ namespace gpopt
 				}
 
 				// cost (use -1 for greedy solutions to ensure we keep all of them)
-				CDouble DCost() { return m_properties.IsGreedy() ? -1.0 : m_cost; }
+				CDouble DCost() { return m_properties.IsGreedy() ? -1.0 : m_cost + m_cost_penalty; }
+
+				CDouble TotalCost() { return m_cost + m_cost_penalty; }
+
 				BOOL ChildrenAreEqual(const SExpressionInfo &other) const
 				{ return m_left_child_expr == other.m_left_child_expr && m_right_child_expr == other.m_right_child_expr; }
 			};
