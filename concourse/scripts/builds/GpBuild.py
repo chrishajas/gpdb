c1e7c635d37b6a3f3829630f2ae2050293ecb890
Chris Hajas <chajas@vmware.com>
2020-07-31 14:35:07 -0700

    more

--- concourse/scripts/builds/GpBuild.py
+++ concourse/scripts/builds/GpBuild.py
@@ -106,7 +106,8 @@ class GpBuild(GpdbBuildBase):
                 with open(output_fname, 'w') as fout:
                     print "Running query: " + fsql
                     current_status = self._run_gpdb_command("env PGOPTIONS='-c optimizer_enable_full_join=on' psql -a -f sql/{}".format(fsql), stdout=fout, stderr=fout, source_env_cmd=source_env_cmd, print_command=False)
-                    print "ERROR: {0}".format(current_status) if current_status != 0
+                    if current_status != 0:
+                        print "ERROR: {0}".format(current_status)
                     status = status if status != 0 else current_status
 
         return status
